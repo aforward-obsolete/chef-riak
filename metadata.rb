905779253b4d63a9bf0231850cb3b2d27b3a0973
Andrew Forward <aforward@gmail.com>
2013-07-15 16:47:33 -0400

    basic chef cookbook

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             "riak"
+maintainer       "Andrew Forward"
+maintainer_email "aforward@gmail.com"
+license          "MIT"
+description      "Configures riak (out of the box unable to get basho to work)."
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
